a58b8f641d89c5f0b670b8c0467c9e86477bdadd
Anton <anton.valpas@aalto.fi>
2025-09-12 20:57:44 +0300

    work

--- notes/node_practice/index.js
+++ notes/node_practice/index.js
@@ -26,10 +26,6 @@ const cors = require('cors')
 
 app.use(cors())
 
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
-})
-
 app.get('/api/notes', (request, response) => {
   response.json(notes)
 })
